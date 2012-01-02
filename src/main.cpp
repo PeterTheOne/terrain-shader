1708cb41e5c9446c6df54d9e4b08bda6409b5471
PeterTheOne <petertheone@gmail.com>
2012-01-02 13:48:08 +0100

    changed context version to 3.3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <GL/glew.h>
 #include <GL/freeglut.h>
 #define WINDOW_TITLE_PREFIX "Chapter 1"
 
@@ -19,19 +18,19 @@ int main(int argc, char* argv[])
 	Initialize(argc, argv);
 
 	glutMainLoop();
-
+	
 	exit(EXIT_SUCCESS);
 }
 
 void Initialize(int argc, char* argv[])
 {
 	InitWindow(argc, argv);
-
+	
 	fprintf(
 		stdout,
 		"INFO: OpenGL Version: %s\n",
 		glGetString(GL_VERSION)
-		);
+	);
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 }
@@ -39,16 +38,16 @@ void Initialize(int argc, char* argv[])
 void InitWindow(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
-
-	glutInitContextVersion(4, 0);
+	
+	glutInitContextVersion(3, 3);
 	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
 
 	glutSetOption(
 		GLUT_ACTION_ON_WINDOW_CLOSE,
 		GLUT_ACTION_GLUTMAINLOOP_RETURNS
-		);
-
+	);
+	
 	glutInitWindowSize(CurrentWidth, CurrentHeight);
 
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
@@ -59,7 +58,7 @@ void InitWindow(int argc, char* argv[])
 		fprintf(
 			stderr,
 			"ERROR: Could not create a new rendering window.\n"
-			);
+		);
 		exit(EXIT_FAILURE);
 	}
 
